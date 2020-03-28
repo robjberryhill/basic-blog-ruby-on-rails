fe012fbae5f06d9809a174dae6fba1bbd4cc1d17
robjberryhill <robjberryhill@gmail.com>
2020-03-28 13:52:40 -0700

    Add article and category association.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,12 @@
 class Article < ActiveRecord::Base
   # This record belongs_to only one user. The user has many articles.
   belongs_to :user
+  # We want the article to have many categories. So we need to do this through an association.
+  # A article has many article_categories model/table.
+  has_many :article_categories
+  # And a article has many categories through the article_categories model/table.
+  # This allows us to have associations with another table through a table article_categories
+  has_many :categories, through: :article_categories
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
+  # We want a category to have many articles. We need to do this through an association from another table.
+  # A category has_many article_categories
+  has_many :article_categories
+  # A category has_many articles through the article_categories model/table association.
+  has_many :articles, through: :article_categories
   # name field must be present, unique, and with min & max rules.
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   # Validates whether the value of the specified attributes are unique across the system.
